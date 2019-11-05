784632a7e913ac14aab5585be9227f9e96654f02
chenye <15002770045@139.com>
2019-11-05 19:05:04 +0800

    seleuim

---
+++ 读取TSV文件并分句北欧.py
@@ -0,0 +1,110 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2019/10/28 11:22
+# @File    : 读取TSV文件并分句.py
+# @Date    : 2019/10/28
+# @Author  : Yuwenjun
+# @Desc    :
+
+from openpyxl import Workbook
+import codecs
+import re
+
+
+letter_regex = re.compile(r'[a-zA-Z]')
+# 加减乘除
+computer_symbol = re.compile(r"[＋－×÷﹢﹣±＝=+-\\]")
+# 去掉这些符号
+special_symbol = re.compile(r"[#'，。★、【】《》？“”‘’！[\]_`{|\u4e00-\u9fa5}~]+")
+# 数字
+num_regex = re.compile(r'[0-9]')
+# 特殊字符 放在另外一个文本
+kuohao_regex = re.compile(r"[<>()?$;]")
+
+
+def clean_tsv_content(contents):
+    """
+    读取tsv文件并分句
+    :param 文本内容
+    :return: list
+    """
+    data = list()
+    for content in contents:
+        e = 0
+        for k, v in enumerate(content):
+            if k == 0:
+                if v == "۔":
+                    e = 1
+            elif k < len(content) - 1:  # 分句
+                # 句号
+                if v == "۔":
+                    if content[k - 1] != "۔" and content[k + 1] != "۔":
+                        if content[k - 1].isdecimal() and content[k + 1].isdecimal():
+                            continue
+                        data.append(content[e:k + 1].strip())
+                        e = k + 1
+                # 问号
+                if v == "؟":
+                    if content[k - 1] != "؟" and content[k + 1] != "؟" and content[k + 1] != ")":
+                        data.append(content[e:k + 1].strip())
+                        e = k + 1
+                # 感叹号
+                if v == "!":
+                    if content[k - 1] != "!" and content[k + 1] != ")":
+                        data.append(content[e:k + 1].strip())
+                        e = k + 1
+                # 分号
+                if v == ";":
+                    data.append(content[e:k + 1].strip())
+                    e = k + 1
+                #
+                if v == "|":
+                    data.append(content[e:k + 1].strip())
+                    e = k + 1
+            else:
+                data.append(content[e:].strip())
+    return data
+
+
+def main(input_path, excel_outpath):
+    """
+    将TXT清洗后的内容，断句写入Excel
+    :param tsv文件路径
+    :return:
+    """
+    wb = Workbook()
+    ws1 = wb.create_sheet("Sheet1")
+    ws2 = wb.create_sheet("需手动筛选的句子")
+    index = 1
+    index2 = 1
+    # with codecs.open(input_path, "r", encoding="utf-8-sig") as f:
+    # with codecs.open(input_path, "r", encoding="gbk") as f:
+    with codecs.open(input_path, "r") as f:
+        contents = f.readlines()
+        datas = clean_tsv_content(contents)
+        for data in datas:
+            #data = data.decode('utf-8')
+            if data is None or len(data) < 15:
+                continue
+            if data.count("،") >= 2:
+                continue
+            if computer_symbol.findall(data):
+                continue
+            if num_regex.findall(data) or kuohao_regex.findall(data):
+                ws2.cell(row=index2, column=1, value=data)
+                index2 += 1
+            ws1.cell(row=index, column=1, value=data)
+            index += 1
+    wb.remove_sheet(wb.get_sheet_by_name("Sheet"))
+    wb.save(excel_outpath)
+
+
+if __name__ == '__main__':
+    #txt_path = input("请输入txt文本路径：")
+    #excel_path = input("请输入excel保存地址：")
+    #txt_path = r"E:\dataset\E-BooksNL-[334]NIEUWSEPTEMBER2019\1Amerge.txt"
+    txt_path = r"E:\dataset\E-BooksNL-[334]NIEUWSEPTEMBER2019\1Smerge.txt"
+    #excel_path = r"E:\dataset\E-BooksNL-[334]NIEUWSEPTEMBER2019\1Amerge.xlsx"
+    excel_path = r"E:\dataset\E-BooksNL-[334]NIEUWSEPTEMBER2019\1Smerge.xlsx"
+    main(txt_path, excel_path)
